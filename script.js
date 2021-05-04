52b307433289ef626ed12a586ab94a58d46d00cd
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-05-04 12:45:20 +0400

    Learn 64. Done.

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 'use strict';
 
+const checkbox = document.querySelector('#checkbox'),
+      form = document.querySelector('form'),
+      change = document.querySelector('#color'),
+      key = 'isChecked',
+      persone = {
+        name: 'Alexandr',
+        age: 105,
+      },
+      serializedPersone = JSON.stringify(persone);
+
+checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+        localStorage.setItem(key, true);
+    } else {
+        localStorage.removeItem(key);
+    }
+});
+
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bg')) {
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = 'white';
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
+
+if (localStorage.getItem(key)) {
+    checkbox.checked = true;
+}
+if (localStorage.getItem('bg')) {
+    form.style.backgroundColor = 'red';
+}
+
+localStorage.setItem('persone', serializedPersone);
+console.log(JSON.parse( localStorage.getItem('persone') ));
